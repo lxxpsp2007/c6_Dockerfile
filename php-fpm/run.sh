d7c8a7100d83b190efe2e44a7d1db20c75915798
lxxpsp2007 <lxxpsp2007@126.com>
2016-12-16 16:25:47 +0800

    add php5.{3 4 6}

--- php-fpm/run.sh
+++ php-fpm/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+php_version=php-5.4.25
 
 if [ ! -z "${fpm_port}" ];then
-    sed -i "s/^listen = .*/listen = 0.0.0.0:${fpm_port}/g" /etc/php-fpm.d/www.conf
+    sed -i "s/^listen = .*/listen = 0.0.0.0:${fpm_port}/g" /usr/local/${php_version}/etc/php-fpm.conf
 fi
 
-php-fpm -F
+/usr/local/${php_version}/sbin/php-fpm -F
