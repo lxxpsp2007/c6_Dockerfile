7b565fc77fc16e0727b5ec24939a58b1f7e5b5c6
lxxpsp2007 <lxxpsp2007@126.com>
2016-12-20 11:32:53 +0800

    add nginx auto reload

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,2 +1,22 @@
 
-/usr/local/nginx-1.10.2/sbin/nginx -g "daemon off;"
+ng_root=/usr/local/nginx-1.10.2
+
+if [ ! -z ${NGINX_VAR} ];then
+    ${ng_root}/sbin/nginx "${NGINX_VAR}" 
+else
+    ${ng_root}/sbin/nginx
+fi
+
+oldcksum=`cksum ${ng_root}/conf/auto_reload`
+
+/usr/local/inotify/bin/inotifywait -e modify,move,create,delete -mr --timefmt '%d/%m/%y %H:%M' --format '%T' \
+${ng_root}/conf/ | while read date time; do
+
+    newcksum=`cksum ${ng_root}/conf/auto_reload`
+    if [ "$newcksum" != "$oldcksum" ]; then
+        #echo "At ${time} on ${date}, config file update detected."
+        oldcksum=$newcksum
+        ${ng_root}/sbin/nginx -s reload
+    fi
+
+done
